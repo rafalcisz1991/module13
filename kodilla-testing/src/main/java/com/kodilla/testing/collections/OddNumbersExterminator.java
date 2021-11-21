873c9107f5116d69c299b7faf72331fe243b5cc5
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2021-11-21 18:23:30 +0100

    6.3 - JUnit, czyli pierwsze testy

--- kodilla-testing/src/main/java/com/kodilla/testing/collections/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collections/OddNumbersExterminator.java
@@ -4,12 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
+    List <Integer> myList = new ArrayList<>();
+    for (int n = 0; n<50; n++){
+        myList.add(n*2);
+
+    }
+
 
     public List<Integer> exterminate (List < Integer > numbers) {
+
         List<Integer> roundNumbers = new ArrayList<>();
         List<Integer> oddNumbers = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            if (i % 2 == 0) {
+        for (int i = 0; i < myList.size(); i++) {
+
+            if ((myList.get(i)) % 2 == 0) {
                 roundNumbers.add(i);
             } else {
                oddNumbers.add(i);
