5424c6c7b270d67ece6e139f226ff011e3829911
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2021-11-21 17:54:56 +0100

    6.3 - JUnit, czyli pierwsze testy

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,51 @@
 package com.kodilla.testing.collection;
 
+import com.kodilla.testing.collections.OddNumbersExterminator;
+import org.junit.jupiter.api.*;
+
 public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before(){
+        System.out.println("Numbers test: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Numbers test: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll (){
+        System.out.println("Collection Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll () {
+        System.out.println("Collection Test Suite: end");
+    }
+
+    @DisplayName(
+            "Round numbers, from automatically created a list of numbers "
+    )
+
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        //When
+
+
+    }
+
+    @Test
+    void testOddNumbersExterminatorNormalList(){
+
+    }
+
+
+
+
+
 }
