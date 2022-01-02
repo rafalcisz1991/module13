837c09bbcf872d26a12aa207121029390a404698
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-02 18:43:09 +0100

    Zadanie 6.7

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -1,22 +1,15 @@
 package com.kodilla.testing.forum.statistics;
-
-
-import com.kodilla.testing.weather.stub.Temperatures;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class StatisticsTestSuite {
-
 
     @Test
     void testAdvStatisticsWith0Posts() {
