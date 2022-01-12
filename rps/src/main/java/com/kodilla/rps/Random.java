1beb8ba1d54bc24d000526a8e039d263938ff56b
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-12 21:18:33 +0100

    Zadanie 7.6

---
+++ rps/src/main/java/com/kodilla/rps/Random.java
@@ -0,0 +1,7 @@
+package com.kodilla.rps;
+
+public class Random {
+
+
+
+}
---
+++ rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -0,0 +1,113 @@
+package com.kodilla.rps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RpsRunner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random randomNumber = new Random();
+        boolean end = false;
+        int playersPick;
+        String playersName;
+        int computerPick;
+        int roundsRequest;
+        int roundsCounter = 1;
+
+        String endGame;
+
+        //RpsRunner rpsRunner = new RpsRunner();
+
+        System.out.println("To begin playing Rock,Paper,Scissors - please state your name:");
+        playersName = scanner.nextLine();
+        System.out.println(" and a duration of the game - number of rounds: ");
+        roundsRequest = scanner.nextInt();
+        System.out.println("In order to pick the following, please press appropriate keyboard button:");
+        System.out.println("'1' for Rock");
+        System.out.println("'2' for Paper");
+        System.out.println("'3' for Scissors");
+
+        while (!end) {
+
+            List<String> playerResults = new ArrayList<>();
+            System.out.println("Round nr " + roundsCounter);
+            System.out.println("Please take your pick: Rock - '1', Paper - '2' or Scissors - '3'");
+            playersPick = scanner.nextInt();
+            computerPick = randomNumber.nextInt(3) + 1;
+
+            //Player's turn
+
+            if (playersPick == 1 || playersPick == 2 || playersPick == 3) {
+                if (playersPick == 1) {
+                    System.out.println(playersName + "picked: Rock");
+                } else if (playersPick == 2) {
+                    System.out.println(playersName + "picked: Paper");
+                } else {
+                    System.out.println(playersName + "picked: Scissors");
+                }
+
+                if (computerPick == 1) {
+                    System.out.println("Computer picked: Rock");
+                    if (playersPick == 2) {
+                        System.out.println(playersName + "wins this round");
+                        playerResults.add("Win");
+                    } else if (playersPick == 1) {
+                        System.out.println("Tie!");
+                    } else {
+                        System.out.println("Computer wins this round!");
+                    }
+                } else if (computerPick == 2) {
+                    System.out.println("Computer picked: Paper");
+                    if (playersPick == 1) {
+                        System.out.println("Computer wins this round!");
+                    } else if (playersPick == 2) {
+                        System.out.println("Tie!");
+                    } else {
+                        System.out.println(playersName + "wins this round!");
+                        playerResults.add("Win");
+                    }
+                } else {
+                    System.out.println("Computer picked: Scissors");
+                    if (playersPick == 1) {
+                        System.out.println(playersName + "wins this round!");
+                        playerResults.add("Win");
+                    } else if (playersPick == 2) {
+                        System.out.println("Computer wins this round!");
+                    } else {
+                        System.out.println("Tie!");
+                    }
+                }
+            } else {
+                System.out.println("You picked wrong");
+                break;
+            }
+            roundsCounter++;
+            if (roundsCounter < roundsRequest) {
+
+            } else {
+                System.out.println("Decide, whether to continue or terminate:");
+                endGame = scanner.nextLine();
+                if (endGame == "x") {
+
+                } else if (endGame == "n") {
+                    System.out.println("You choose to continue");
+                } else {
+                    System.out.println("Game finished");
+                }
+                System.out.println("You chose to terminate");
+                end = false;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
