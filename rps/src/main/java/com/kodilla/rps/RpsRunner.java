d21dfdef542985b0d45c61db742d53a2c31235c2
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-12 21:49:59 +0100

    Zadanie 7.7

--- rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -32,64 +32,64 @@ public class RpsRunner {
         System.out.println("'3' for Scissors");
 
 
-            while (!end) {
+        while (!end) {
 
-                List<String> playerResults = new ArrayList<>();
-                System.out.println("Round nr " + roundsCounter);
-                System.out.println("Please take your pick: Rock - '1', Paper - '2' or Scissors - '3'");
-                playersPick = scanner.nextInt();
-                computerPick = randomNumber.nextInt(3);
+            List<String> playerResults = new ArrayList<>();
+            System.out.println("Round nr " + roundsCounter);
+            System.out.println("Please take your pick: Rock - '1', Paper - '2' or Scissors - '3'");
+            playersPick = scanner.nextInt();
+            computerPick = randomNumber.nextInt(3);
 
-                if (playersPick == 1 || playersPick == 2 || playersPick == 3) {
-                    if (playersPick == 1) {
-                        System.out.println(playersName + "picked: Rock");
-                    } else if (playersPick == 2) {
-                        System.out.println(playersName + "picked: Paper");
+            if (playersPick == 1 || playersPick == 2 || playersPick == 3) {
+                if (playersPick == 1) {
+                    System.out.println(playersName + "picked: Rock");
+                } else if (playersPick == 2) {
+                    System.out.println(playersName + "picked: Paper");
+                } else {
+                    System.out.println(playersName + "picked: Scissors");
+                }
+
+                if (computerPick == 1) {
+                    System.out.println("Computer picked: Rock");
+                    if (playersPick == 2) {
+                        System.out.println(playersName + "wins this round");
+                        playerResults.add("Win");
+                    } else if (playersPick == 1) {
+                        System.out.println("Tie!");
                     } else {
-                        System.out.println(playersName + "picked: Scissors");
+                        System.out.println("Computer wins this round!");
                     }
-
-                    if (computerPick == 1) {
-                        System.out.println("Computer picked: Rock");
-                        if (playersPick == 2) {
-                            System.out.println(playersName + "wins this round");
-                            playerResults.add("Win");
-                        } else if (playersPick == 1) {
-                            System.out.println("Tie!");
-                        } else {
-                            System.out.println("Computer wins this round!");
-                        }
-                    } else if (computerPick == 2) {
-                        System.out.println("Computer picked: Paper");
-                        if (playersPick == 1) {
-                            System.out.println("Computer wins this round!");
-                        } else if (playersPick == 2) {
-                            System.out.println("Tie!");
-                        } else {
-                            System.out.println(playersName + "wins this round!");
-                            playerResults.add("Win");
-                        }
+                } else if (computerPick == 2) {
+                    System.out.println("Computer picked: Paper");
+                    if (playersPick == 1) {
+                        System.out.println("Computer wins this round!");
+                    } else if (playersPick == 2) {
+                        System.out.println("Tie!");
                     } else {
-                        System.out.println("Computer picked: Scissors");
-                        if (playersPick == 1) {
-                            System.out.println(playersName + "wins this round!");
-                            playerResults.add("Win");
-                        } else if (playersPick == 2) {
-                            System.out.println("Computer wins this round!");
-                        } else {
-                            System.out.println("Tie!");
-                        }
+                        System.out.println(playersName + "wins this round!");
+                        playerResults.add("Win");
                     }
                 } else {
-                    System.out.println("You picked wrong");
-                    break;
+                    System.out.println("Computer picked: Scissors");
+                    if (playersPick == 1) {
+                        System.out.println(playersName + "wins this round!");
+                        playerResults.add("Win");
+                    } else if (playersPick == 2) {
+                        System.out.println("Computer wins this round!");
+                    } else {
+                        System.out.println("Tie!");
+                    }
                 }
-            }
-
-
+            } else {
+                System.out.println("You picked wrong");
+                break;
             }
         }
     }
+}
+
+
+
 
 
 
