cfda2bcfa1b9908923f831fd8e74a1f257efe8c9
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-13 20:48:54 +0100

    Zadanie 7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -10,17 +10,18 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage(){
         //Given
-        int[] numbers = new int[5];
+        int[] numbers = new int[6];
         numbers[0] = 4;
         numbers[1] = 10;
         numbers[2] = 21;
         numbers[3] = 13;
         numbers[4] = 100;
+        numbers[5] = 47;
 
         //When
         double resultAverage = ArrayOperations.getAverage(numbers);
 
         //Then
-        assertEquals(29.6, resultAverage);
+        assertEquals(32.5, resultAverage);
     }
 }
