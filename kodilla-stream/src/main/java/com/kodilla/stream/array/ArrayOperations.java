620f0ce7a3bf87efbfec471e5215fca5d60d1c7a
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-13 20:43:55 +0100

    Zadanie 7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,7 +1,5 @@
 package com.kodilla.stream.array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
@@ -9,16 +7,12 @@ public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
 
-        /*int minValue = numbers
-        int maxValue = num*/
-
-
-        IntStream.range(1, 20)
-                .sorted()
-                .filter(w -> w % 2 == 0)
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .forEach(System.out::println);
 
-        OptionalDouble myAverage = IntStream.range(numbers[0], numbers.length)
+        OptionalDouble myAverage = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .average();
 
         return myAverage.getAsDouble();
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -10,15 +10,16 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage(){
         //Given
-        int numbers[] = new int[20];
-        for(int i = 0; i < numbers.length; i++) {
-            numbers[i] = i + 1;
-        }
+        int[] numbers = new int[4];
+        numbers[0] = 4;
+        numbers[1] = 10;
+        numbers[2] = 21;
+        numbers[3] = 13;
 
         //When
         double resultAverage = ArrayOperations.getAverage(numbers);
 
         //Then
-        assertEquals(10, resultAverage);
+        assertEquals(12, resultAverage);
     }
 }
