727dcc74823630b61e50d872a7b92099fc827f90
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-22 21:15:47 +0100

    Zadanie 13.2

--- good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Seller.java
+++ good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Seller.java
@@ -4,11 +4,17 @@ import java.util.HashMap;
 
 public class Seller extends User{
 
-    HashMap <Product, Integer> productsInStock;
+    public HashMap <Product, Integer> productsInStock;
 
     public Seller(String name, String email, HashMap<Product, Integer> productsInStock) {
         super(name, email);
         this.productsInStock = productsInStock;
+    }
+
+    public HashMap<Product, Integer> availableProducts (Product product, int quantity) {
+        productsInStock.put(product, quantity);
+
+        return productsInStock;
     }
 }
 
