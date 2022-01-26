4654bd61aa37bbbc0861d1ec1b658d35c2e85ee1
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-26 12:56:49 +0100

    Zadanie 13.4

--- good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie134/Application.java
+++ good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie134/Application.java
@@ -5,12 +5,12 @@ public class Application {
 
         Company1 company1 = new Company1("comp1");
         Company2 company2 = new Company2("comp2");
-        Products products = new Products("Fruits");
+        Products products1 = new Products("Fruits");
+        Products products2 = new Products("Pastries");
 
         CompanyProcessor companyProcessor = new CompanyProcessor();
 
-        companyProcessor.purchaseApp(company1, products, 20);
-
-
+        companyProcessor.purchaseApp(company1, products1, 20);
+        companyProcessor.purchaseApp(company2, products2, 40);
     }
 }
--- good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie134/CompanyProcessor.java
+++ good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie134/CompanyProcessor.java
@@ -3,12 +3,6 @@ package com.kodilla.good.patterns.challenges.zadanie134;
 public class CompanyProcessor {
 
     public void purchaseApp(Company company, Products products, int quantity){
-        if (company.getName().equals("comp1")){
-            company.process(products, quantity);
-        } else if (company.getName().equals("Comp2")){
-            company.process(products, quantity);
-        } else {
-            System.out.println("No such shop at our service.");
-        }
+        company.process(products, quantity);
     }
 }
