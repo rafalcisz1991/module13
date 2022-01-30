30bc2f0934557e52af2998910dc350207071ca67
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-01-30 21:09:46 +0100

    Zadanie 13.5

--- good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie135/Application.java
+++ good-patterns/src/main/java/com/kodilla/good/patterns/challenges/zadanie135/Application.java
@@ -1,35 +1,9 @@
 
 package com.kodilla.good.patterns.challenges.zadanie135;
 
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args){
-
-        FlightSeeker flightSeeker = new FlightSeeker();
-
-        List<String> transferAirports = new ArrayList<>();
-        transferAirports.add("Okecie");
-        transferAirports.add("Modlin");
-        transferAirports.add("De Gaulle");
-        FlightWithTransfer flightWithTransfer = new FlightWithTransfer("LAX",
-                transferAirports, "Schiphol");
-
-        HashSet<FlightWithTransfer> transferFlightsList = new HashSet<>();
-        transferFlightsList.add(flightWithTransfer);
-
-        System.out.println("Flights below");
-        System.out.println(flightSeeker.getFlightRoute("LAX", transferFlightsList));
-
-
-        System.out.println("More flights below");
-        System.out.println(flightWithTransfer.getAllAirports());
-
-
     }
 }
 
