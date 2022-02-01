837ce1e7a86372b39e381d997e58c110b094536a
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-02-01 19:52:22 +0100

    Zadanie 14.2

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    @Autowired
+    private Display display;
+
+    public double add(double a, double b) {
+        System.out.println("Number:");
+        display.displayValue(a);
+        System.out.println("will be added to: ");
+        display.displayValue(b);
+        return a+b;
+    }
+
+    public double sub(double a, double b) {
+        display.displayValue(a);
+        display.displayValue(b);
+        return a-b;
+    }
+
+    public double mul(double a, double b) {
+        display.displayValue(a);
+        display.displayValue(b);
+        return a*b;
+    }
+
+    public double div(double a, double b) {
+        display.displayValue(a);
+        display.displayValue(b);
+        return a/b;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double addResult = calculator.add(2, 2);
+        double subResult = calculator.sub(4, 2);
+        double mulResult = calculator.mul(2, 2);
+        double divResult = calculator.div(4, 2);
+
+        //Then
+        assertEquals(4, addResult);
+        assertEquals(2, subResult);
+        assertEquals(4, mulResult);
+        assertEquals(2, divResult);
+    }
+}
+
+
