eaa037c40a06d846dda4dac57eefb185594d7916
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-02-05 15:10:16 +0100

    Zadanie 14.3

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -5,17 +5,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootTest
+
 public class BoardTestSuite {
 
     @Test
     void testTaskAdd() {
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Board board = context.getBean(Board.class);
 
-        //When
-        board
+        //When & Then
+        board.getToDoList();
+        board.getDoneList();
+        board.getInProgressList();
 
 
 
