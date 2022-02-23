f632f10e7a2fbc9d9b09ded8ff6cac5f570ef6a5
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2022-02-23 16:48:46 +0100

    Zadanie 17.4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TaskFinancialDetailsDaoTestSuite {
 
