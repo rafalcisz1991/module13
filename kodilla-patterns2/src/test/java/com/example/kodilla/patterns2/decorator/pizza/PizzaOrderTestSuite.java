33d6eff134e83dc3184081eb7ef5944e92d9b8dd
rafalcisz1991 <rafal.cisz1991@gmail.com>
2022-05-23 15:57:24 +0200

    Zadanie 24.2 - poprawki testów

--- kodilla-patterns2/src/test/java/com/example/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/example/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -9,68 +9,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PizzaOrderTestSuite {
 
     @Test
-    public void testBasicPizzaOrderGetCost() {
+    public void testBasicPizzaOrderGetCostAndDescription() {
         //Given
-        PizzaOrder theOrder = new BasicPizzaOrder();
-        //When
-        BigDecimal calculatedCost = theOrder.getCost();
-        //Then
-        assertEquals(new BigDecimal("15.00"), calculatedCost);
-    }
 
-    @Test
-    public void testBasicPizzaOrderGetDescription() {
-        //Given
-        PizzaOrder theOrder = new BasicPizzaOrder();
         //When
-        String description = theOrder.getDescription();
+        PizzaOrder theOrder = new BasicPizzaOrder();
+
         //Then
+        BigDecimal calculatedCost = theOrder.getCost();
+        String description = theOrder.getDescription();
+        assertEquals(new BigDecimal("15.00"), calculatedCost);
         assertEquals("Basic dough, tomato sauce, cheese", description);
     }
 
     @Test
-    public void testHawaiiPizzaToppingsGetCost() {
+    public void testHawaiiPizzaToppingsGetCostAndDescription() {
         //Given
-        PizzaOrder theOrder = new BasicPizzaOrder();
-        theOrder = new HawaiiToppingsPizzaOrderDecorator(theOrder);
-        //When
-        BigDecimal theCost = theOrder.getCost();
-        //Then
-        assertEquals(new BigDecimal("20.00"), theCost);
-    }
 
-    @Test
-    public void testHawaiiPizzaToppingsGetDescription() {
-        //Given
+        //When
         PizzaOrder theOrder = new BasicPizzaOrder();
         theOrder = new HawaiiToppingsPizzaOrderDecorator(theOrder);
-        //When
-        String description = theOrder.getDescription();
+
         //Then
+        BigDecimal theCost = theOrder.getCost();
+        String description = theOrder.getDescription();
+        assertEquals(new BigDecimal("20.00"), theCost);
         assertEquals("Basic dough, tomato sauce, cheese, pineapple", description);
     }
 
     @Test
-    public void testHawaiiAndCheesePizzaToppingsGetCost() {
+    public void testHawaiiAndCheesePizzaToppingsGetCostAndDescription() {
         //Given
-        PizzaOrder theOrder = new BasicPizzaOrder();
-        theOrder = new HawaiiToppingsPizzaOrderDecorator(theOrder);
-        theOrder = new CheeseToppingsPizzaOrderDecorator(theOrder);
-        //When
-        BigDecimal theCost = theOrder.getCost();
-        //Then
-        assertEquals(new BigDecimal("30.00"), theCost);
-    }
 
-    @Test
-    public void testHawaiiAndCheesePizzaToppingsGetDescription() {
-        //Given
+        //When
         PizzaOrder theOrder = new BasicPizzaOrder();
         theOrder = new HawaiiToppingsPizzaOrderDecorator(theOrder);
         theOrder = new CheeseToppingsPizzaOrderDecorator(theOrder);
-        //When
-        String description = theOrder.getDescription();
+
         //Then
+        BigDecimal theCost = theOrder.getCost();
+        String description = theOrder.getDescription();
+        assertEquals(new BigDecimal("30.00"), theCost);
         assertEquals("Basic dough, tomato sauce, cheese, pineapple, cheddar, mozzarella", description);
     }
 }
